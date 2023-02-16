6a8a434b3bc13c21fa7c89d3e4c47b628be3c985
tchinandeyobaka <lookgoodzen@gmail.com>
2023-02-16 17:39:39 +0800

    fix warning

--- src/class/GetData.js
+++ src/class/GetData.js
@@ -1,12 +1,32 @@
-import axios from "axios"
-class GetData {
- constructor(){
-    axios.get('https://api.github.com/users').then((res)=>{
-    this.data=res.data;
- }).catch((err)=>{
-    return err
- })
- }
-}
+import React from 'react';
 
-export default new GetData
+import axios from 'axios';
+
+export default class GetData extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+  view(){
+    return this.state.persons.map((d,i)=>{
+        return(
+            <div className="wrapper" key={i}>
+                <h1 className="Content-title">
+                    {d.login}
+                </h1>
+                <img className="Content-image" src={d.avatar_url} alt="ryu" />
+            </div>
+        ) 
+    })
+  }
+  render() {
+    return this.view()
+   }
+}
--- src/components/ViewData.jsx
+++ src/components/ViewData.jsx
@@ -1,16 +1,22 @@
 import GetData from "../class/GetData"
 const ViewData = () =>{
-    let list_data  = GetData.data;
-    return list_data.map((d,i)=>{
-        return(
-            <div className="wrapper" key={i}>
-                <h1 className="Content-title">
-                    {d.login}
-                </h1>
-                <img className="Content-image" src={d.avatar_url} alt="ryu" />
-            </div>
-        ) 
-    })
+
+    return(
+        <>
+            <GetData/>
+        </>
+    )
+    // let list_data  = GetData.data;
+    // return list_data.map((d,i)=>{
+    //     return(
+    //         <div className="wrapper" key={i}>
+    //             <h1 className="Content-title">
+    //                 {d.login}
+    //             </h1>
+    //             <img className="Content-image" src={d.avatar_url} alt="ryu" />
+    //         </div>
+    //     ) 
+    // })
 
 }
 
